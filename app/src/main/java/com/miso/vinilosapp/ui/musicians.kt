4be9fc4294feaf14f9eee9de435ab63a2c167f31
LauraSilvaAyala <lm.silvaa1@uniandes.edu.co>
2021-11-10 22:32:28 -0500

    Adiciòn de conexion a la api

--- app/src/main/java/com/miso/vinilosapp/ui/musicians.kt
+++ app/src/main/java/com/miso/vinilosapp/ui/musicians.kt
@@ -1,10 +1,11 @@
-package com.miso.vinilosapp
+package com.miso.vinilosapp.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.miso.vinilosapp.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
