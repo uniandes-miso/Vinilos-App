566e2931a77fd3de2e77b26af9d07885d13a71f6
Wilson Beltran <beltran@ingenieros.com>
2021-11-20 15:34:08 -0500

    Detalle album

--- app/src/main/java/com/miso/vinilosapp/ui/fragment_albums_detail.kt
+++ app/src/main/java/com/miso/vinilosapp/ui/fragment_albums_detail.kt
@@ -1,16 +1,21 @@
 package com.miso.vinilosapp.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
 import com.miso.vinilosapp.R
+import com.miso.vinilosapp.models.Album
+import com.miso.vinilosapp.ui.adapters.AlbumsAdapter
+import com.miso.vinilosapp.viewmodels.AlbumViewModel
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -19,14 +24,14 @@ private const val ARG_PARAM2 = "param2"
  */
 class fragment_albums_detail : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var albumId: Int? = null
+    private var albumSelected: Album? = null
+    private lateinit var viewModel: AlbumViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+            albumId = it.getInt("albumId")
         }
     }
 
@@ -34,26 +39,37 @@ class fragment_albums_detail : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val activity = requireNotNull(this.activity) {
+            "You can only access the viewModel after onActivityCreated()"
+        }
+        viewModel = ViewModelProvider(this, AlbumViewModel.Factory(activity.application)).get(AlbumViewModel::class.java)
+        //albumSelected =
+        Log.d("Album detail Size albums ", listOf(viewModel.albums).size.toString())
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_albums_detail, container, false)
+        //textAlbumName
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("Albums Detail" , albumId.toString())
+        var tAlbumName : TextView = view.findViewById<TextView>(R.id.textAlbumName)
+        tAlbumName.setText("Si yo supiera que me voy a morir mañana")
+        var tAlbumDescription : TextView = view.findViewById<TextView>(R.id.textAlbumDescription)
+        tAlbumDescription.setText("Album creado por silvestre")
+        var tAlbumGenre : TextView = view.findViewById<TextView>(R.id.textAlbumGenre)
+        tAlbumGenre.setText("Vallenato")
+        var tAlbumRecordLabel : TextView = view.findViewById<TextView>(R.id.textAlbumRecordLabel)
+        tAlbumGenre.setText("Sony music")
 
+
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fragment_albums_detail.
-         */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
+        fun newInstance(albumId: Int) =
             fragment_albums_detail().apply {
                 arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                    putString(ARG_PARAM1, albumId.toString())
                 }
             }
     }
